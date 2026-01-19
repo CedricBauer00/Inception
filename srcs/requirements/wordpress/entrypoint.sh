298cb8d733f6578a205e7822e445204c7cc51dcb
cbauer <cbauer@student.42heilbronn.de>
2026-01-19 16:24:42 +0100

    created user and user admin, cleaned .env file; rework

--- srcs/requirements/wordpress/entrypoint.sh
+++ srcs/requirements/wordpress/entrypoint.sh
@@ -1,13 +1,20 @@
 #!/bin/bash
 echo "Waiting for MariaDB to be ready..."
-while ! mysqladmin ping -h"$WORDPRESS_DB_HOST" --silent; do
-    sleep 1
-done
-echo "MariaDB is ready!"
+# while ! mysqladmin ping -h"$WORDPRESS_DB_HOST" --silent; do
+#     sleep 1
+# done
+# echo "MariaDB is ready!"
+
+
+sleep 5
+
+cd /var/www/html
 
 # wordpress configuration creating; if not existent
 if [ ! -f wp-config.php ]; then
     echo "Creating WordPress configuration..."
+
+    wp core download --allow-root
 
     # creating wp-config.php from ep-config-sample.php
     cp wp-config-sample.php wp-config.php
@@ -18,7 +25,22 @@ if [ ! -f wp-config.php ]; then
     sed -i "s/password_here/$DB_PASSWORD/g" wp-config.php
     sed -i "s/localhost/mariadb/g" wp-config.php
 
-    echo "WordPress configuration created!"
+    wp core install \
+        --url=$DOMAIN_NAME \
+        --title="Inception" \
+        --admin_user=$WP_ADMIN_USER \
+        --admin_password=$WP_ADMIN_PASSWORD \
+        --admin_email=$WP_ADMIN_EMAIL \
+        --allow-root
+
+    wp user create \
+        $WP_USER \
+        $WP_EMAIL \
+        --role=author \
+        --user_pass=$wp_password \
+        --allow-root
+
+    echo "WordPress installed!"
 fi
 
 # set permissions
