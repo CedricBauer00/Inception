cffce27c9d5d3692f3b96f13d4d8b8b6af95106b
CedricBauer00 <bauercedric91@gmail.com>
2026-03-12 09:33:05 +0100

    trying to implement redis cache; health check research;

---
+++ srcs/requirements/bonus/FTPserver/entrypoint.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+#set -e #aborts script if command fails
+
+if ! id -u ${FTP_USER} > /dev/null 2>&1; then #if user id not existing
+    useradd -d /var/www/html ${FTP_USER} #create user to home (-m), '-d /var/.../html' -> to wordpress folder
+    echo "${FTP_USER}:${FTP_PASSWORD}" | chpasswd # set password
+    chown -R ${FTP_USER}:${FTP_USER} /var/www/html #grants FTP-user permission for all wordpress data -- recursivly to all subdirectories
+fi
+
+mkdir -p /var/run/vsftpd/empty
+
+cat > /etc/vsftpd.conf <<EOF #create .conf, opens heredoc
+listen=YES
+listen_port=21
+anonymous_enable=NO
+local_enable=YES
+write_enable=YES
+local_umask=022
+chroot_local_user=YES
+allow_writeable_chroot=YES
+pasv_enable=YES
+pasv_min_port=21100
+pasv_max_port=21110
+userlist_enable=YES
+userlist_file=/etc/vsftpd.userlist
+userlist_deny=NO
+seccomp_sandbox=NO
+EOF
+
+echo "${FTP_USER}" > /etc/vsftpd.userlist # user put to white list - only he can login
+
+echo "FTP server starting..."
+
+# tail -f /dev/null
+
+# # exec /usr/sbin/vsftpd /etc/vsftpd.conf #start process
+
+vsftpd /etc/vsftpd.conf
--- srcs/requirements/mariadb/init.sh
+++ srcs/requirements/mariadb/init.sh
@@ -1,37 +1,39 @@
 #!/bin/bash
-set -e
+set -e #if exit code != 0 - abort
 
 echo "Starting MariaDB initialisation..."
 
-if [ ! -d "/var/lib/mysql/mysql" ]; then
+if [ ! -d "/var/lib/mysql/mysql" ]; then # folder der internen Datenbank (safes Users, permissions, ...) - if not existing, mariadb has never been initialised
     echo "Initializing MariaDB..."
-    mysql_install_db --user=mysql --datadir=/var/lib/mysql > /dev/null
+    mysql_install_db --user=mysql --datadir=/var/lib/mysql > /dev/null 
 fi
+#--user=mysql creates database as linux user, not as root
+# --datadir=/var/lib/mysql safes database files in this folder
 
-service mariadb start
+service mariadb start # start, so SQL commands can be executed - process runs in background 
 
-while ! mysqladmin ping --silent 2>/dev/null; do
+while ! mysqladmin ping --silent 2>/dev/null; do # checks if Mariadb is ready
     sleep 1
 done
 
 echo "setting up database and user..."
 
-echo "CREATE DATABASE IF NOT EXISTS ${MYSQL_DATABASE};" >> db.sql
-# echo "CREATE USER IF NOT EXISTS 'root'@'%' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';" >> db.sql 
-# echo "GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' WITH GRANT OPTION;" >> db.sql
-echo "CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';" >> db.sql 
-echo "GRANT ALL PRIVILEGES ON ${MYSQL_DATABASE}.* TO '${MYSQL_USER}'@'%';" >> db.sql
-# echo "SET PASSWORD FOR 'root'@'localhost' = PASSWORD('${MYSQL_ROOT_PASSWORD}');" >> db.sql
+echo "CREATE DATABASE IF NOT EXISTS ${MYSQL_DATABASE};" >> db.sql # creates wordpress database if not existing
+echo "CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';" >> db.sql # creates database User, '@'%' -> everyone can connect, not only localhost
+echo "GRANT ALL PRIVILEGES ON ${MYSQL_DATABASE}.* TO '${MYSQL_USER}'@'%';" >> db.sql # grants permissions to database
 echo "FLUSH PRIVILEGES;" >> db.sql
 
-mysql -u root < db.sql
+mysql -u root < db.sql # SQL commands redirect into mysql - execute as root 
 rm -f db.sql
 
 sleep 2
-service mariadb stop
+service mariadb stop # stops for clean structure - will be restartet in exec
 sleep 2
 
-exec mysqld_safe --datadir=/var/lib/mysql
+exec mysqld_safe --datadir=/var/lib/mysql # process runs as PID1
+
+
+
 
 # docker exec -it mariadb bash
 # mysql -u root
